fc7556391944c729cf59ae2315cef475d52ff2ff
Michael Hungbo <hungbomichael@gmail.com>
2023-07-08 08:16:09 +0100

    update styles

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -3,28 +3,28 @@ const Chart = () => {
     <>
     <section>
     <div className="flex m-4 gap-2">
-      <div className="flex-1 px-2 justify-center w-16 bg-gray-600 shadow rounded h-300px">
+      <div className="flex-1 px-2 justify-center w-16 bg-gray-700 shadow rounded h-300px">
         <div className="">
         <p className="text-gray-900 font-bold">Total returns</p>
         <p className="py-4 font-bold">$30,000 </p>
         <p className="text-green-300">+34.5%</p>
         </div>            
       </div>
-      <div className="flex-1 px-2 justify-center w-16 bg-gray-600 shadow rounded max-h-300px">
+      <div className="flex-1 px-2 justify-center w-16 bg-gray-700 shadow rounded max-h-300px">
       <div className="">
         <p className="text-gray-900 font-bold">Total sales</p>
         <p className="py-4 font-bold">$30,000 </p>
         <p className="text-green-300">+34.5%</p>
         </div>
       </div>
-      <div className="flex-1 px-2 justify-center w-16  bg-gray-600 shadow rounded max-h-300px">
+      <div className="flex-1 px-2 justify-center w-16  bg-gray-700 shadow rounded max-h-300px">
       <div className="">
         <p className="text-gray-900 font-bold">Total subscriptions</p>
         <p className="py-4 font-bold">$30,000 </p>
         <p className="text-green-300">+34.5%</p>
         </div>
       </div>
-      <div className="flex-1 px-2 justify-center w-16  bg-gray-600 shadow rounded h-300px">
+      <div className="flex-1 px-2 justify-center w-16  bg-gray-700 shadow rounded h-300px">
         <div className="">
         <p className="text-gray-900 font-bold">Total returns</p>
         <p className="py-4 font-bold ">$30,000 </p>
@@ -32,8 +32,17 @@ const Chart = () => {
         </div>            
       </div>
     </div>    
-
     </section>
+
+        <section className="flex my-4 px-4 gap-3">
+          <div className="w-2/4 h-[300px] bg-gray-700 rounded">
+
+          </div>
+
+          <div className="w-2/4 h-[300px] bg-gray-700 rounded">
+
+          </div>
+        </section>
     </>
 )
 }
