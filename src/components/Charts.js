406b1d2e9dc4bf905affb8ac34e948dc4a29032e
Michael Hungbo <hungbomichael@gmail.com>
2023-07-11 07:14:06 +0100

    refactor chart components

--- src/components/Charts.js
+++ src/components/Charts.js
@@ -48,8 +48,8 @@ const Charts = () => {
 
       <section className="flex my-4 px-4 gap-2">
         <div className=" w-1/3 h-[250px] bg-gray-700 rounded"><PieChartPlot/></div>
-        <div className=" w-1/3 h-[250px] bg-gray-700 rounded"></div>
-        <div className=" w-1/3 h-[250px] bg-gray-700 rounded"></div>
+        <div className=" w-1/3 h-[250px] bg-gray-700 rounded"><LineChartPlot/></div>
+        <div className=" w-1/3 h-[250px] bg-gray-700 rounded"><RadarChartPlot/></div>
       </section>
     </>
   );
